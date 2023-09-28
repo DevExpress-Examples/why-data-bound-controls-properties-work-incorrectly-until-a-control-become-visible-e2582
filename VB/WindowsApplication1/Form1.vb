cd3d66b55856ae63d0bfa0e70c27b002d7a18d7d
DevExpressExampleBot <fakeEmail>
2023-09-28 12:31:11 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,69 +1,63 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
+    Public Partial Class Form1
+        Inherits Form
 
-		Private _GridControl As GridControl
-		Private _ShowSolution As Boolean
+        Private _GridControl As GridControl
 
-			Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Name", GetType(String))
-			tbl.Columns.Add("ID", GetType(Integer))
-			tbl.Columns.Add("Number", GetType(Integer))
-			tbl.Columns.Add("Date", GetType(DateTime))
-			For i As Integer = 0 To RowCount - 1
-				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, DateTime.Now.AddDays(i) })
-			Next i
-			Return tbl
-			End Function
+        Private _ShowSolution As Boolean
 
+        Private Function CreateTable(ByVal RowCount As Integer) As DataTable
+            Dim tbl As DataTable = New DataTable()
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Date", GetType(Date))
+            For i As Integer = 0 To RowCount - 1
+                tbl.Rows.Add(New Object() {String.Format("Name{0}", i), i, 3 - i, Date.Now.AddDays(i)})
+            Next
 
-		Public Sub New(ByVal showSolution As Boolean)
-			_ShowSolution = showSolution
-			InitializeComponent()
-			Dim tbl As DataTable = CreateTable(3)
-			textBox1.DataBindings.Add("Text", tbl, "Name")
-			textBox2.DataBindings.Add("Text", tbl, "Name")
-			_GridControl = New GridControl()
-			If showSolution Then
-				_GridControl.BindingContext = New BindingContext()
-			End If
-			_GridControl.MainView = New GridView(_GridControl)
-			_GridControl.DataSource = tbl
-			_GridControl.Dock = DockStyle.Bottom
-			_GridControl.Height = 200
+            Return tbl
+        End Function
 
-		End Sub
+        Public Sub New(ByVal showSolution As Boolean)
+            _ShowSolution = showSolution
+            InitializeComponent()
+            Dim tbl As DataTable = CreateTable(3)
+            textBox1.DataBindings.Add("Text", tbl, "Name")
+            textBox2.DataBindings.Add("Text", tbl, "Name")
+            _GridControl = New GridControl()
+            If showSolution Then
+                _GridControl.BindingContext = New BindingContext()
+            End If
 
+            _GridControl.MainView = New GridView(_GridControl)
+            _GridControl.DataSource = tbl
+            _GridControl.Dock = DockStyle.Bottom
+            _GridControl.Height = 200
+        End Sub
 
-		Private Sub timer1_Elapsed(ByVal sender As Object, ByVal e As System.Timers.ElapsedEventArgs) Handles timer1.Elapsed
-			label1.Text = String.Format("textBox1.Text = {0}", textBox1.Text)
-			label2.Text = String.Format("textBox1.Text = {0}", textBox2.Text)
-			label3.Text = String.Format("gridView1.DataRowCount = {0}", (TryCast(_GridControl.MainView, GridView)).DataRowCount)
-		End Sub
+        Private Sub timer1_Elapsed(ByVal sender As Object, ByVal e As Timers.ElapsedEventArgs)
+            label1.Text = String.Format("textBox1.Text = {0}", textBox1.Text)
+            label2.Text = String.Format("textBox1.Text = {0}", textBox2.Text)
+            label3.Text = String.Format("gridView1.DataRowCount = {0}", TryCast(_GridControl.MainView, GridView).DataRowCount)
+        End Sub
 
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			panel2.Controls.Add(_GridControl)
-		End Sub
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            panel2.Controls.Add(_GridControl)
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			If _ShowSolution Then
-				tabControl1.SelectedIndex = 1
-			End If
-			tabControl1.SelectedIndex = 0
-		End Sub
-
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            If _ShowSolution Then tabControl1.SelectedIndex = 1
+            tabControl1.SelectedIndex = 0
+        End Sub
+    End Class
 End Namespace
